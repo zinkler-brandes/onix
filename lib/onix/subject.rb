8485e9201d05d16913ff19015a60deb4c8c91a7e
Björn Zinkler <bz@zinkler-brandes.com>
2017-08-07 10:55:35 +0300

    attribute rename

--- lib/onix/subject.rb
+++ lib/onix/subject.rb
@@ -6,7 +6,7 @@ module ONIX
 
     xml_name "MainSubject"
 
-    xml_accessor :subject_scheme_id,      :from => "SubjectSchemeIdentifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
+    xml_accessor :main_subject_scheme_id, :from => "MainSubjectSchemeIdentifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :subject_scheme_name,    :from => "SubjectSchemeName"
     xml_accessor :subject_scheme_version, :from => "SubjectSchemeVersion"
     xml_accessor :subject_code,           :from => "SubjectCode"
