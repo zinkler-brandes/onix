ec958302b95f06b1307af7faf6db5ada1e0b5798
Björn Zinkler <bz@zinkler-brandes.com>
2017-08-07 11:27:39 +0300

    Update Publisher Attributes

--- lib/onix/publisher.rb
+++ lib/onix/publisher.rb
@@ -8,8 +8,7 @@ module ONIX
 
     xml_accessor :publishing_role,      :from => "PublishingRole", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :name_code_type,       :from => "NameCodeType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :name_code_type_name,  :from => "NameCodeTypeName"
-    xml_accessor :name_code_type_value, :from => "NameCodeTypeValue"
+    xml_accessor :name_code_value,      :from => "NameCodeValue"
     xml_accessor :publisher_name,       :from => "PublisherName"
   end
 end
