5348311292eca20cee382c92841209e26acd956f
Björn Zinkler <bz@zinkler-brandes.com>
2017-08-08 10:40:38 +0300

    +Subjects

--- lib/onix/main_subject.rb
+++ lib/onix/main_subject.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 module ONIX
-  class Subject
+  class MainSubject
     include ROXML
 
-    xml_name "Subject"
+    xml_name "MainSubject"
 
-    xml_accessor :subject_scheme_id,      :from => "SubjectSchemeIdentifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
+    xml_accessor :main_subject_scheme_id, :from => "MainSubjectSchemeIdentifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :subject_scheme_name,    :from => "SubjectSchemeName"
     xml_accessor :subject_scheme_version, :from => "SubjectSchemeVersion"
     xml_accessor :subject_code,           :from => "SubjectCode"
