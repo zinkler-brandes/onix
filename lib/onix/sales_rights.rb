5896ce150083446c885a3a1d613b018c80413a99
Björn Zinkler <bz@zinkler-brandes.com>
2019-05-08 12:06:15 +0200

    no message

--- lib/onix/sales_rights.rb
+++ lib/onix/sales_rights.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 module ONIX
-  class SalesRight
+  class SalesRights
     include ROXML
 
-    xml_name "SalesRight"
+    xml_name "SalesRights"
 
     xml_accessor :sales_rights_type, :from => "SalesRightsType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :rights_territory, :from => "RightsTerritory"
